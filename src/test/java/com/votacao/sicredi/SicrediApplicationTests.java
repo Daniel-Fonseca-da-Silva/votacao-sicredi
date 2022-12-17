dc18a91b9860fb812ca18fe625704fb063238ecf
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-12-17 18:00:23 -0300

    feat: Projeto importado

---
+++ src/test/java/com/votacao/sicredi/SicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votacao.sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
