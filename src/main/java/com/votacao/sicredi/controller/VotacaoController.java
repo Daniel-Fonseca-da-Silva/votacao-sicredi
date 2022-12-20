177aebf748b68b2d96b79562b521f210bd2d9f80
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-12-20 20:46:45 -0300

    refactor: Adicionado injecao do repository e implementado metodo get

--- src/main/java/com/votacao/sicredi/controller/VotacaoController.java
+++ src/main/java/com/votacao/sicredi/controller/VotacaoController.java
@@ -1,15 +1,24 @@
 package com.votacao.sicredi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.votacao.sicredi.repository.PautaRepository;
 
 @Controller
 @RequestMapping(path = "/pautas")
 public class VotacaoController {
 	
+	@Autowired
+	private PautaRepository repository;
+	
 	@GetMapping()
-	public String buscarPauta() {
+	public String buscarPauta(Model model) {
+		
+		model.addAttribute("pautas", repository.buscaTodasAsPautas());
 		return "pautas/home";
 	}
 
