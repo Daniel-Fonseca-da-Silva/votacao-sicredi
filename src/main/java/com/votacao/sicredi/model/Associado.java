6847e1cb226861cee9d3b08021d79a947f1b57ef
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-12-21 00:04:09 -0300

    feat: Criado modelo associado

---
+++ src/main/java/com/votacao/sicredi/model/Associado.java
@@ -0,0 +1,20 @@
+package com.votacao.sicredi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Associado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String cargo;
+
+}
