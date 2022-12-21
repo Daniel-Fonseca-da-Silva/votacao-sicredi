14167a07ca6562dffeed7680cff63031d3be569c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-12-21 00:00:40 -0300

    feat: Criado controller para associado

---
+++ src/main/java/com/votacao/sicredi/controller/AssociadoController.java
@@ -0,0 +1,24 @@
+package com.votacao.sicredi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.votacao.sicredi.repository.AssociadoRepository;
+
+@Controller
+@RequestMapping(path = "/associado")
+public class AssociadoController {
+	
+	@Autowired
+	private AssociadoRepository repository;
+	
+	@GetMapping()
+	public String buscarAssociado(Model model) {
+		model.addAttribute("associados", repository.buscaTodosAssociados());
+		return "associados/home";
+	}
+
+}
