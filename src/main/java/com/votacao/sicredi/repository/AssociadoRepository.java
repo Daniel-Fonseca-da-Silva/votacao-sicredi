e977415adca47abd0b26d30f443e814027943e67
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-12-23 15:22:52 -0300

    refactor: Modificado repositorios para o uso do spring data

--- src/main/java/com/votacao/sicredi/repository/AssociadoRepository.java
+++ src/main/java/com/votacao/sicredi/repository/AssociadoRepository.java
@@ -1,24 +1,11 @@
 package com.votacao.sicredi.repository;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.votacao.sicredi.model.Associado;
 
 @Repository
-public class AssociadoRepository {
-	
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public List<Associado> buscaTodosAssociados() {
-		Query query = entityManager.createQuery("SELECT a FROM Associado a", Associado.class);
-		return query.getResultList();
-	}
+public interface AssociadoRepository extends JpaRepository<Associado, Long>{
 
 }
--- src/main/java/com/votacao/sicredi/repository/PautaRepository.java
+++ src/main/java/com/votacao/sicredi/repository/PautaRepository.java
@@ -6,18 +6,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.votacao.sicredi.model.Pauta;
 
 @Repository
-public class PautaRepository {
+public interface PautaRepository extends JpaRepository<Pauta, Long> {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
-	
-	public List<Pauta> buscaTodasAsPautas() {
-		Query query = entityManager.createQuery("SELECT p FROM Pauta p", Pauta.class);
-		return query.getResultList();
-	}
 }
