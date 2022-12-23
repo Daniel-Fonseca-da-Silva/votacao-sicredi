97642ef70e58a41120755a69cbcdf8a2c5180e29
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-12-23 15:19:40 -0300

    feat: Criado endpoint para salvar formulario de associados

--- src/main/java/com/votacao/sicredi/controller/AssociadoController.java
+++ src/main/java/com/votacao/sicredi/controller/AssociadoController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.votacao.sicredi.dto.AssociadoDTO;
+import com.votacao.sicredi.model.Associado;
 import com.votacao.sicredi.repository.AssociadoRepository;
 
 @Controller
-@RequestMapping(path = "/associado")
+@RequestMapping("associado")
 public class AssociadoController {
 	
 	@Autowired
@@ -17,13 +20,20 @@ public class AssociadoController {
 	
 	@GetMapping()
 	public String buscarAssociado(Model model) {
-		model.addAttribute("associados", repository.buscaTodosAssociados());
+		model.addAttribute("associados", repository.findAll());
 		return "associados/home";
 	}
 	
 	@GetMapping("formulario")
 	public String formulario() {
 		return "associados/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(AssociadoDTO associadoDTO) {
+		Associado associado = associadoDTO.toAssociado();
+		repository.save(associado);
+		return "associados/home";
 	}
 
 }
