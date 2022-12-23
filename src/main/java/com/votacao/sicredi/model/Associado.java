02ef1b86385210f9a27ad6d295dfe7fb3c88c0b6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-12-23 15:21:52 -0300

    refactor: Criado relacionamento em associado model

--- src/main/java/com/votacao/sicredi/model/Associado.java
+++ src/main/java/com/votacao/sicredi/model/Associado.java
@@ -1,9 +1,12 @@
 package com.votacao.sicredi.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -13,10 +16,13 @@ public class Associado {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long idAssociado;
 	private String nome;
 	private String cargo;
 	private String email;
 	private String senha;
+	
+	@OneToMany(mappedBy = "associado")
+	private List<Pauta> pautas;
 
 }
